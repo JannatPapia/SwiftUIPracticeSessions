79ac2a02a127794eaaa1e40d0de2899182a1984f
Jannatun Nahar Papia <papiamcode@Jannatuns-MacBook-Pro.local>
2022-02-07 17:53:26 +0600

    Found Error

--- BarChartSwiftUI/BarChartSwiftUI/ContentView.swift
+++ BarChartSwiftUI/BarChartSwiftUI/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  BarChartSwiftUI
+//  BarChartUI
 //
 //  Created by Jannatun Nahar Papia  on 7/2/22.
 //
@@ -8,9 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var pickerSelectedItem = 0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        // use ZStack for background color
+        ZStack {
+            
+            Color.pink.edgesIgnoringSafeArea(.all)
+            // use VStack for weekday line
+            VStack {
+                
+        Text("Calory Intake")
+           // .padding()
+        // Modify size
+            .font(.system(size: 34))
+            .fontWeight(.heavy) // use heavy font
+            
+            Picker(selection: $pickerSelectedItem, label: Text("")) {
+                Text("Weekday").tag(0) // use tag for highlight Text
+                Text("Afternoon").tag(1)
+                Text("Evening").tag(2)
+            }.pickerStyle(SegmentedPickerStyle())
+                //add some padding value horizontally
+                padding(.horizontal, 24)
+                
+                HStack {
+                    ZStack {
+                        Capsule().frame(width: 30, height: 200)
+                        Capsule().frame(width: 30, height: 100)
+                            .foregroundColor(.white)
+                    }
+                }
+        }
+        }
     }
 }
 
