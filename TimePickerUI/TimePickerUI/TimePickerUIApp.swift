f10bdbde43b4aaf0759c93ef6c3de7df0aab2b6e
Jannatun Nahar Papia <papiamcode@Jannatuns-MacBook-Pro.local>
2022-02-07 15:02:09 +0600

    Start TimerPicker Project

---
+++ TimePickerUI/TimePickerUI/TimePickerUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimePickerUIApp.swift
+//  TimePickerUI
+//
+//  Created by Jannatun Nahar Papia  on 7/2/22.
+//
+
+import SwiftUI
+
+@main
+struct TimePickerUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
