1222e1ea1f262be70efc97769bc0dec3bfd5d472
Jannatun Nahar Papia <papiamcode@Jannatuns-MacBook-Pro.local>
2022-02-07 17:48:24 +0600

    Setup Project BarChartUI

---
+++ BarChartSwiftUI/BarChartSwiftUI/BarChartSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarChartSwiftUIApp.swift
+//  BarChartSwiftUI
+//
+//  Created by Jannatun Nahar Papia  on 7/2/22.
+//
+
+import SwiftUI
+
+@main
+struct BarChartSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
