229c911af451c28933dbe6ad94fbcfe90319b6fd
Jannatun Nahar Papia <papiamcode@Jannatuns-MacBook-Pro.local>
2022-02-07 15:09:49 +0600

    Setup project WaterAnimation

---
+++ WaterAnimation/WaterAnimation/WaterAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterAnimationApp.swift
+//  WaterAnimation
+//
+//  Created by Jannatun Nahar Papia  on 7/2/22.
+//
+
+import SwiftUI
+
+@main
+struct WaterAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
